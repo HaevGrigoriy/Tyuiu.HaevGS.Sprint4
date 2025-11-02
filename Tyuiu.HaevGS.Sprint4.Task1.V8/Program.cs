26fdc5e73ff5a19b9069134302a9cf8e1bdff60d
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-02 18:35:14 +0500

    Task 1

--- Tyuiu.HaevGS.Sprint4.Task1.V8/Program.cs
+++ Tyuiu.HaevGS.Sprint4.Task1.V8/Program.cs
@@ -2,13 +2,35 @@
 
 DataService ds = new DataService();
 
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
 int len;
+Console.WriteLine("Введите количество элементов массива: ");
 len = Convert.ToInt32(Console.ReadLine());
 
-int[] num = new int[len];
+int[] numsArray = new int[len];
 
-for (int i = 0; i < len; i++)
+for (int i = 0; i <= len - 1; i++)
 {
-    num[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите значение " + i + "элемента массива: ");
+    numsArray[i] = Convert.ToInt32(Console.ReadLine());
 }
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.Write(numsArray[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(numsArray);
+Console.WriteLine(res);
+
 Console.ReadKey();
