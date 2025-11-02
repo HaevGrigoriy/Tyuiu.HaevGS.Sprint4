951a8a49716f432d3833b8232e1890a4da842e4f
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-02 18:26:41 +0500

    Task 1

--- Tyuiu.HaevGS.Sprint4.Task1.V8/Program.cs
+++ Tyuiu.HaevGS.Sprint4.Task1.V8/Program.cs
@@ -1,1 +1,14 @@
 ﻿using Tyuiu.HaevGS.Sprint4.Task1.V8.Lib;
+
+DataService ds = new DataService();
+
+int len;
+len = Convert.ToInt32(Console.ReadLine());
+
+int[] num = new int[len];
+
+for (int i = 0; i < len-1; i++)
+{
+    num[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.ReadKey();
