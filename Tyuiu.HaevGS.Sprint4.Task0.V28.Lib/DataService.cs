d7c5faf43d396d192f6cad5db70df6ba8c60937e
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-02 17:56:04 +0500

    Add project files.

---
+++ Tyuiu.HaevGS.Sprint4.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.HaevGS.Sprint4.Task0.V28.Lib
+{
+    public class DataService : ISprint4Task0V28
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int count = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count *= array[i];
+                }
+            }
+            return count;
+        }
+    }
+}
