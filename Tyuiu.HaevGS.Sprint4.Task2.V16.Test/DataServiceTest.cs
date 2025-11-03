39a93494a1cc6df5db7422e45f500c33028d3a6a
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-03 22:07:53 +0500

    Task 2

---
+++ Tyuiu.HaevGS.Sprint4.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.HaevGS.Sprint4.Task2.V16.Lib;
+
+namespace Tyuiu.HaevGS.Sprint4.Task2.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint4.Task2.V16/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.HaevGS.Sprint4.Task2.V16.Lib;
+
+
+DataService ds =  new DataService();
+
+int len;
+Console.WriteLine("Введите количество элементов массива: ");
+len = Convert.ToInt32(Console.ReadLine());
+
+int[] numsArray = new int[len];
+
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.Write("Введите значение " + i + "элемента массива: ");
+    numsArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.Write(numsArray[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(numsArray);
+Console.WriteLine(res);
+
+Console.ReadKey();
